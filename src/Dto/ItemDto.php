fc2f6215927f863ebb64dc20b2b20c82ffb58158
Aleksejs <aleks4444@inbox.lv>
2022-05-28 10:24:01 +0300

    extend item dto

--- src/Dto/ItemDto.php
+++ src/Dto/ItemDto.php
@@ -68,6 +68,95 @@ class ItemDto
             return null;
         }
 
-        return (int) (new \DateTime())->diff($this->buyDate)->format('%m');
+        $diff = (new \DateTime())->diff($this->buyDate);
+
+        return ((int) $diff->format('%m')) + (((int) $diff->format('%y')) * 12);
+    }
+
+    public function getMonthsInUseString(): ?string
+    {
+        if (!$this->buyDate) {
+            return null;
+        }
+
+        $diff = (new \DateTime())->diff($this->buyDate);
+        $months = (string) (((int) $diff->format('%m')) + (((int) $diff->format('%y')) * 12));
+
+        return $months.'.'.(round((int) $diff->format('%d') / 30));
+    }
+
+    public function getMonthPrice(): ?string
+    {
+        $monthsInUse = $this->getMonthsInUse();
+        if ($this->price && $this->buyDate && null !== $monthsInUse && $monthsInUse > 0) {
+            return (string) round((float) $this->price / $monthsInUse, 2);
+        }
+
+        return null;
+    }
+
+    public function getExpireAfter(): ?int
+    {
+        $monthsInUse = $this->getMonthsInUse();
+        if ($this->buyDate && $this->planToUseInMonths && null !== $monthsInUse) {
+            return $this->planToUseInMonths - $monthsInUse;
+        }
+
+        return null;
+    }
+
+    public function getPlanMonthValue(): ?string
+    {
+        if ($this->price && $this->planToUseInMonths) {
+            return (string) round((float) $this->price / $this->planToUseInMonths, 2);
+        }
+
+        return null;
+    }
+
+    public function getCurrentValue(): ?string
+    {
+        $monthsInUse = $this->getMonthsInUse();
+        if (null !== $this->price
+            && null !== $this->planToUseInMonths
+            && null !== $this->buyDate
+            && null !== $monthsInUse
+        ) {
+            $secondHandTotalPrice = (float) $this->price * 0.85;
+            $usagePrice = $secondHandTotalPrice / $this->planToUseInMonths * $monthsInUse;
+
+            return (string) round(max($secondHandTotalPrice - $usagePrice, 0), 2);
+        }
+
+        return null;
+    }
+
+    public function getCanChange(): ?string
+    {
+        if ($this->planToUseInMonths && $this->buyDate) {
+            return $this->planToUseInMonths <= $this->getMonthsInUse() ? $this->getPrice() : null;
+        }
+
+        return null;
+    }
+
+    public function getTotalYears(): ?string
+    {
+        $monthsInUse = $this->getMonthsInUse();
+        if ($monthsInUse) {
+            return (string) round($monthsInUse / 12, 1);
+        }
+
+        return null;
+    }
+
+    public function getExtraYears(): ?string
+    {
+        $monthsInUse = $this->getMonthsInUse();
+        if ($monthsInUse && $this->planToUseInMonths) {
+            return (string) round(($monthsInUse - $this->planToUseInMonths) / 12, 1);
+        }
+
+        return null;
     }
 }
